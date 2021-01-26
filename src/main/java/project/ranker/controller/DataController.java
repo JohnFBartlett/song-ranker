e83a7eb58e51a7ed7cefcc8c6ec2834d8f2f6cf6
John Bartlett <jfbart252@gmail.com>
2021-01-25 21:49:39 -0500

    change endpoint

--- src/main/java/project/ranker/controller/DataController.java
+++ src/main/java/project/ranker/controller/DataController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("data")
+@RequestMapping("rankData")
 public class DataController {
 	
 	@Autowired
