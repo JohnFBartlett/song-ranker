3176332fe43dd8fefcc23fddc8834a629602867a
John Bartlett <jfbart252@gmail.com>
2021-01-27 23:17:22 -0500

    local path

--- src/main/resources/angular-ranker/src/app/services/hero.service.ts
+++ src/main/resources/angular-ranker/src/app/services/hero.service.ts
@@ -21,9 +21,9 @@ export class HeroService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  private categoriesUrl = 'http://127.0.0.1:9001/rankData/categories'; // URL to web api
-  private optionsUrl = 'http://127.0.0.1:9001/rankData/options'; // URL to web api
-  private rankUrl = 'http://127.0.0.1:9001/ranking';
+  private categoriesUrl = '/rankData/categories'; // URL to web api
+  private optionsUrl = '/rankData/options'; // URL to web api
+  private rankUrl = '/ranking';
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
